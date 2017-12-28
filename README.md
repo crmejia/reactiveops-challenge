a5972de02e511cbc4f8d4f2dce910cb35cb1c0d2
Crismar Mejia <crismar.mejia@gmail.com>
2017-12-28 18:09:35 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+### milestones
+
+* Write a bash script that provisions an ec2 instance with rails
+* Script install terraform and checks AWS keys are present
+* Add Datadog agent?
---
+++ rails_deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! $(command -v terraform) ]
+then
+  echo Please install terraform
+  exit 1
+fi
+ if [ ! -z "$AWS_ACCESS_KEY_ID" ] ; then
+   echo Please provide a valid AWS access key enviroment variable
+   echo name should be AWS_ACCESS_KEY_ID
+   exit 1
+ fi
+
+ if [ ! -z "$AWS_SECRET_ACCESS_KEY" ] ; then
+   echo Please provide a valid AWS secret Key enviroment variable
+   echo name should be AWS_SECRET_ACCESS_KEY
+   exit 1
+ fi
